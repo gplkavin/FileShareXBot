c5440d52fbfb79f6e8b48e31143873e39fbf25f9
NesamaniX1 <77948100+NesamaniX1@users.noreply.github.com>
2023-01-18 09:00:17 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -1,10 +1,10 @@
 #(©)Codexbotz
 
 import asyncio
-from shortlink import get_shortlink
 from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
+from utils import get_shortlink
 
 from bot import Bot
 from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON , SITE , API_KEY
