3e9d355fb15743f4d92181499f47e0666bf5f6cf
DineshBalasubramaniam04 <122980603+DineshBalasubramaniam04@users.noreply.github.com>
2023-02-11 12:57:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,8 +28,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 PORT = os.environ.get("PORT", "8080")
 
 #shortlink
-SHORTENER_API = os.environ.get("SHORTENER_API", "1aab74171e9891abd0ba799e3fd568c9598a79e1")
-SHORTENER_SITE = os.environ.get("SHORTENER_SITE", "droplink.co")
+SHORTENER_API = os.environ.get("SHORTENER_API", "")
+SHORTENER_SITE = os.environ.get("SHORTENER_SITE", "")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "")
