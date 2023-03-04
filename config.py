32fe11e8148194f02522c1ef8f6b46a971d12beb
gplkavin <81810902+gplkavin@users.noreply.github.com>
2023-03-04 10:33:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,40 +11,40 @@ from logging.handlers import RotatingFileHandler
 from dotenv import load_dotenv
 load_dotenv()
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5844147844:AAGBe6C-5dr606pBbGB3JMNTWdcCffFTlwc")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = int(os.environ.get("APP_ID", "28352139"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "cbfcf7bb291cd885db00f4a25f5c7127")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001159368239"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1174794359"))
 
 #Port
 PORT = os.environ.get("PORT", "8080")
 
 #shortlink
-SHORTENER_API = os.environ.get("SHORTENER_API")
-SHORTENER_SITE = os.environ.get("SHORTENER_SITE")
+SHORTENER_API = os.environ.get("52485ddd93ca298fd075e8979a5889721ca48f75")
+SHORTENER_SITE = os.environ.get("tnlink.in")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://gplkavin:gplkavin@cluster0.nzdd34t.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001509652119"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
-    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "").split())]
+    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "1245135578 1389078939 1337528464").split())]
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.") 
 
